e775523833711ae855022387978242c0f8804e9c
Zack Wu <hi@zackwu.com>
2023-12-10 21:40:33 +0800

    feat: solutions for day 2

---
+++ day02a/src/main.rs
@@ -0,0 +1,62 @@
+use std::io::BufRead;
+
+fn color_to_count(color: &str) -> u32 {
+    match color {
+        "red" => 12,
+        "green" => 13,
+        "blue" => 14,
+        _ => 0,
+    }
+}
+
+// Format of a line: Game 1: 3 blue, 4 red; 1 red, 2 green, 6 blue; 2 green
+fn check_game(line: &str) -> u32 {
+    let mut parts = line.split([';', ':', ',']);
+    let game_number = parts.next().unwrap(); // something like "Game 1"
+    let game_number: u32 = game_number.split(" ").last().unwrap().parse().unwrap(); // something like 1
+    fn enough_cubes(cubes: &str) -> bool {
+        let count_and_color = cubes.trim().split(" ").collect::<Vec<&str>>();
+        let count = count_and_color[0].parse::<u32>().unwrap();
+        count <= color_to_count(count_and_color[1])
+    }
+    if parts.all(enough_cubes) {
+        game_number
+    } else {
+        0
+    }
+}
+
+#[test]
+fn test_check_game() {
+    assert_eq!(check_game("Game 1: 1 green, 1 blue, 1 red; 3 green, 1 blue, 1 red; 4 green, 3 blue, 1 red; 4 green, 2 blue, 1 red; 3 blue, 3 green"), 1);
+    assert_eq!(check_game("Game 2: 9 blue, 7 red; 5 blue, 6 green, 1 red; 2 blue, 10 red, 9 green; 3 green, 14 red, 5 blue; 8 red, 3 blue, 4 green; 8 green, 14 blue, 10 red"), 0);
+    assert_eq!(check_game("Game 3: 11 green, 8 blue, 7 red; 3 green, 4 red, 9 blue; 3 red, 4 green, 8 blue; 11 green, 1 red, 16 blue"), 0);
+    assert_eq!(check_game("Game 4: 3 blue, 20 green, 2 red; 1 green, 3 red, 3 blue; 1 blue, 9 green; 4 red, 17 green; 12 green, 3 red"), 0);
+    assert_eq!(
+        check_game("Game 5: 2 red, 1 blue, 4 green; 6 blue, 2 green; 2 red, 5 green"),
+        5
+    );
+}
+
+fn process_lines_in_file(filename: &str) -> u32 {
+    let file = std::fs::File::open(filename).unwrap();
+    let reader = std::io::BufReader::new(file);
+    let mut sum = 0;
+    for line in reader.lines() {
+        let line = line.unwrap();
+        sum += check_game(&line);
+    }
+    sum
+}
+
+#[test]
+fn test_process_lines_in_file() {
+    assert_eq!(process_lines_in_file("./src/input.txt"), 2377);
+}
+
+fn main() {
+    let args: Vec<String> = std::env::args().collect();
+    let filename = &args[1];
+    let sum = process_lines_in_file(filename);
+    println!("Sum of possible game IDs in {filename}: {}", sum);
+}
---
+++ day02b/src/main.rs
@@ -0,0 +1,71 @@
+use std::io::BufRead;
+
+fn power_of_cubes(line: &str) -> u32 {
+    let without_game_id = line[line.find(':').unwrap() + 1..].trim();
+    let mut color_to_count = std::collections::HashMap::new();
+
+    without_game_id.split(';').for_each(|colors_and_counts| {
+        colors_and_counts.split(',').for_each(|color_and_count| {
+            let count_and_color = color_and_count.trim().split(" ").collect::<Vec<&str>>();
+            let count = count_and_color[0].parse::<u32>().unwrap();
+            let color = count_and_color[1];
+            color_to_count
+                .entry(color)
+                .and_modify(|e| *e = std::cmp::max(*e, count))
+                .or_insert(count);
+        });
+    });
+    if color_to_count.len() == 3 {
+        color_to_count.values().fold(1, |acc, count| acc * count)
+    } else {
+        // if some color is missing, then by definition, we only need zero of that color and the product is zero
+        0
+    }
+}
+
+#[test]
+fn test_power_of_cubes() {
+    assert_eq!(
+        power_of_cubes("Game 1: 3 blue, 4 red; 1 red, 2 green, 6 blue; 2 green"),
+        4 * 2 * 6
+    );
+    assert_eq!(
+        power_of_cubes("Game 2: 1 blue, 2 green; 3 green, 4 blue, 1 red; 1 green, 1 blue"),
+        1 * 3 * 4
+    );
+    assert_eq!(
+        power_of_cubes("Game 3: 8 green, 6 blue, 20 red; 5 blue, 4 red, 13 green; 5 green, 1 red"),
+        20 * 13 * 6
+    );
+    assert_eq!(
+        power_of_cubes("Game 4: 1 green, 3 red, 6 blue; 3 green, 6 red; 3 green, 15 blue, 14 red"),
+        14 * 3 * 15
+    );
+    assert_eq!(
+        power_of_cubes("Game 5: 6 red, 1 blue, 3 green; 2 blue, 1 red, 2 green"),
+        6 * 3 * 2
+    );
+}
+
+fn process_lines_in_file(filename: &str) -> u32 {
+    let file = std::fs::File::open(filename).unwrap();
+    let reader = std::io::BufReader::new(file);
+    let mut sum = 0;
+    for line in reader.lines() {
+        let line = line.unwrap();
+        sum += power_of_cubes(&line);
+    }
+    sum
+}
+
+#[test]
+fn test_process_lines_in_file() {
+    assert_eq!(process_lines_in_file("./src/input.txt"), 71220);
+}
+
+fn main() {
+    let args: Vec<String> = std::env::args().collect();
+    let filename = &args[1];
+    let sum = process_lines_in_file(filename);
+    println!("Sum of possible game IDs in {filename}: {}", sum);
+}
